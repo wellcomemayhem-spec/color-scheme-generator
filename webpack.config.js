2298f69f239ccbc0e6a0dad8fd00ae9a1bb98912
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-29 10:03:46 +0800

    Add comments to clarify webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require('path');
 
 module.exports = {
+    // Entry point of the application
     entry: './src/index.js',
     output: {
+        // Output directory and filename for the bundled code
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
         rules: [{
+            // Rule to process JavaScript files
             test: /\.js$/, // for JavaScript files
             exclude: /node_modules/, 
             use: {
                 loader: 'babel-loader',
                 options: {
+                    // Babel presets for modern JavaScript and React
                     presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             }
         }]
     },
     devServer: {
+        // Serve static files from the dist directory
         static: path.join(__dirname, 'dist'),
+        // Enable compression for files served
         compress: true,
+        // Port number for the development server
         port: 3000
     }
 };

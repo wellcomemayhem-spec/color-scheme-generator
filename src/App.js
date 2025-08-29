0324298db38635ecd71fe0f5377aa8ec2e200459
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-29 10:00:55 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ColorPalette from './ColorPalette';
+
+function App() {
+    return (
+        <div>
+            <h1>Color Scheme Generator</h1>
+            <ColorPalette />
+        </div>
+    );
+}
+
+export default App;
